c5112709371cafeaaeb81522249f3713c43dba6d
a.m. hasler <amhasler@gmail.com>
2013-05-12 20:35:57 -0400

    Finish user edit, update, index, and destroy actions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -56,7 +56,7 @@ describe "Static pages" do
     click_link "Home"
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
-    click_link "sample app"
+    click_link "Project Page One"
     page.should have_selector 'title', text: full_title('')
   end
 end
